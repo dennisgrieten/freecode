1d3fe37cec0d8c6eec70546c1bc8f109a00317d9
Dennis Grieten <dennisgrieten@gmail.com>
2016-10-23 13:19:52 +0200

    busy

--- advanced algorithm scripting/no_repeats_please.js
+++ advanced algorithm scripting/no_repeats_please.js
@@ -1,5 +1,39 @@
 function permAlone(str) {
-    return str;
+    str = str.split("");
+    var permArr = [];
+    var usedChars = [];
+
+    function permute(input) {
+        var i, ch;
+        for (i = 0; i < input.length; i++) {
+            ch = input.splice(i, 1)[0];
+            usedChars.push(ch);
+            if (input.length == 0) {
+                permArr.push(usedChars.slice());
+            }
+            permute(input);
+            input.splice(i, 0, ch);
+            usedChars.pop();
+        }
+        return permArr
+    }
+
+    function countNonConsecutive(str) {
+        var regex = /([a-z])[1]/g;
+        var count = 0;
+
+        for (var i = 0; i < str.length; i++) {
+            console.log(str[i].join(""));
+            if (regex.test(str[i].join(""))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    return countNonConsecutive(permute(str));
 }
 
-permAlone('aab');
+
+console.log(permAlone('aab'));
